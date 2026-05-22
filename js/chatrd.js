14204229b13b84b5a9509a436525752655e63ed0
Rodrigo Emanuel <xrodrigolima@gmail.com>
2026-05-21 23:07:28 -0300

    Major Code Revamp

--- js/chatrd.js
+++ js/chatrd.js
@@ -46,10 +46,10 @@ const loadedEmotes = new Set();
 
 /* ✅ Explicit whitelist */
 const SKINS = {
-    default: "skin-default.css?nocache=41",
-    nutting: "skin-nutting.css?nocache=41",
-    kimballs: "skin-kimballs.css?nocache=41",
-    bubbles: "skin-bubbles.css?nocache=41"
+    default: "skin-default.css?nocache=43",
+    nutting: "skin-nutting.css?nocache=43",
+    kimballs: "skin-kimballs.css?nocache=43",
+    bubbles: "skin-bubbles.css?nocache=43"
 };
 
 const skinFile = SKINS[chatrdSkin] || SKINS.default;
@@ -71,6 +71,8 @@ if (chatHorizontal == true) {
     chatContainer.classList.remove('oneline');
     chatContainer.classList.add('horizontal');
 }
+
+if (!chatHorizontal && !chatOneLine) { chatContainer.classList.add('vertical'); }
 
 let backgroundColor = hexToRGBA(chatBackground,chatBackgroundOpacity);
 chatContainer.parentElement.style.backgroundColor = backgroundColor;
