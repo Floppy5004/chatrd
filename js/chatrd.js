3f342510013e1dd178fd7766c2aa8d72559db952
Rodrigo Emanuel <xrodrigolima@gmail.com>
2026-05-22 02:39:54 -0300

    D:<

--- js/chatrd.js
+++ js/chatrd.js
@@ -46,10 +46,10 @@ const loadedEmotes = new Set();
 
 /* ✅ Explicit whitelist */
 const SKINS = {
-    default: "skin-default.css?nocache=09",
-    nutting: "skin-nutting.css?nocache=09",
-    kimballs: "skin-kimballs.css?nocache=09",
-    bubbles: "skin-bubbles.css?nocache=09"
+    default: "skin-default.css?nocache=10",
+    nutting: "skin-nutting.css?nocache=10",
+    kimballs: "skin-kimballs.css?nocache=10",
+    bubbles: "skin-bubbles.css?nocache=10"
 };
 
 const skinFile = SKINS[chatrdSkin] || SKINS.default;
